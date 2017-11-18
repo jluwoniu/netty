dddf0e986944feb4f3b3c7ab8a7762b310a9b37b
jluwoniu <jluwoniu@outlook.com>
2017-11-19 01:21:14 +0800

    codec

--- codec/src/main/java/com/jluwoniu/netty/handler/HttpServerHandler.java
+++ codec/src/main/java/com/jluwoniu/netty/handler/HttpServerHandler.java
@@ -3,7 +3,6 @@ package com.jluwoniu.netty.handler;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.DecoderResult;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
@@ -13,22 +12,19 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpReque
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
 
-        //System.out.println("httpServerHandler received message");
         String uri = msg.uri();
         String method = msg.method().toString();
-        HttpHeaders headers = msg.headers();
         HttpVersion version = msg.protocolVersion();
-        ByteBuf content = msg.content();
         StringBuffer sb = new StringBuffer();
         sb.append("HTTP VERSION:").append(version.toString()).append(" ");
         sb.append("method:").append(method).append(" ");
         sb.append("uri:").append(uri);
         System.out.println(sb.toString());
-        HttpVersion httpVersion = new HttpVersion("HTTP",1,1,false);
         ByteBuf body = ctx.alloc().buffer().writeBytes("<html><body>hello netty</body></html>".getBytes(CharsetUtil.UTF_8));
-        DefaultFullHttpResponse response = new DefaultFullHttpResponse(httpVersion,HttpResponseStatus.OK,body);
-
-        //ctx.channel().writeAndFlush(body);
+        int lenght = body.readableBytes();
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK,body);
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,lenght);
+        response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
         ctx.writeAndFlush(response);
     }
 }
