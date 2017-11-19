09194645fdff112c0e9e6946ec563c92428216c7
jluwoniu <jluwoniu@outlook.com>
2017-11-19 20:35:17 +0800

    format

--- websocket/src/main/java/com/jluwoniu/netty/handler/HttpRequestHandler.java
+++ websocket/src/main/java/com/jluwoniu/netty/handler/HttpRequestHandler.java
@@ -15,21 +15,18 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     private final String webSocketUrl;
     private final static File index;
 
-    static{
+    static {
         URL location = HttpRequestHandler.class.getProtectionDomain().getCodeSource().getLocation();
         try {
             String path = location.toURI() + "index.html";
             path = !path.contains("file:") ? path : path.substring(5);
             index = new File(path);
-        }
-        catch (URISyntaxException e)
-        {
-            throw new IllegalStateException("Unable to locate index.html",e);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Unable to locate index.html", e);
         }
     }
 
-    public HttpRequestHandler(String webSocketUrl)
-    {
+    public HttpRequestHandler(String webSocketUrl) {
         this.webSocketUrl = webSocketUrl;
     }
 
@@ -37,44 +34,35 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
 
-        if(webSocketUrl.equalsIgnoreCase(msg.uri()))
-        {
+        if (webSocketUrl.equalsIgnoreCase(msg.uri())) {
             ctx.fireChannelRead(msg);
-        }
-        else
-        {
-            if(HttpUtil.is100ContinueExpected(msg))
-            {
+        } else {
+            if (HttpUtil.is100ContinueExpected(msg)) {
                 send100Continue(ctx);
             }
-            RandomAccessFile file = new RandomAccessFile(index,"r");
-            HttpResponse response = new DefaultFullHttpResponse(msg.protocolVersion(),HttpResponseStatus.OK);
-            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/html; charset=UTF-8");
+            RandomAccessFile file = new RandomAccessFile(index, "r");
+            HttpResponse response = new DefaultFullHttpResponse(msg.protocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
             boolean isKeepAlive = HttpUtil.isKeepAlive(msg);
-            if(isKeepAlive)
-            {
-                response.headers().set(HttpHeaderNames.CONTENT_LENGTH,file.length());
-                response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
+            if (isKeepAlive) {
+                response.headers().set(HttpHeaderNames.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
             }
             ctx.write(response);
-            if(ctx.pipeline().get(SslHandler.class) == null)//没有使用ssl加密
+            if (ctx.pipeline().get(SslHandler.class) == null)//没有使用ssl加密
             {
-                ctx.write(new DefaultFileRegion(file.getChannel(),0,file.length()));
-            }
-            else
-            {
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            } else {
                 ctx.write(new ChunkedNioFile(file.getChannel()));
             }
             ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
-            if(!isKeepAlive)
-            {
+            if (!isKeepAlive) {
                 future.addListener(ChannelFutureListener.CLOSE);
             }
         }
     }
 
-    private static void send100Continue(ChannelHandlerContext ctx)
-    {
+    private static void send100Continue(ChannelHandlerContext ctx) {
         FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
         ctx.writeAndFlush(response);
     }
